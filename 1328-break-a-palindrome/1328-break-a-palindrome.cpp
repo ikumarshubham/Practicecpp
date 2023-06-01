de506bf8aa3f2bdce096167989486aca611cf4e6
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-06-01 17:50:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.55%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        
+        if(palindrome.size()==1||palindrome.size()==0)return "";
+        int n=palindrome.size();
+        string s1=palindrome;
+        int i;
+for( i=0 ;i<n;i++){
+    if(s1[i]!='a'){
+        char ch=s1[i];
+        s1[i]='a';
+        string s2=s1;
+        reverse(s2.begin(),s2.end());
+        if(s1==s2){
+            s1[i]=ch;
+            continue;
+        }
+        else if(s1!=s2){
+            break;
+        }
+                  }
+   
+}
+       if(i==n)(s1[n-1]='b');
+        return s1;
+    }
+};
