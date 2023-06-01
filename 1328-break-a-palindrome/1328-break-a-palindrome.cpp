6dcbb03fdccfdf9760a3a1d6684ed88ac8b597b0
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-06-01 18:07:06 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (52.55%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -4,25 +4,36 @@ public:
         
         if(palindrome.size()==1||palindrome.size()==0)return "";
         int n=palindrome.size();
-        string s1=palindrome;
-        int i;
-for( i=0 ;i<n;i++){
-    if(s1[i]!='a'){
-        char ch=s1[i];
-        s1[i]='a';
-        string s2=s1;
-        reverse(s2.begin(),s2.end());
-        if(s1==s2){
-            s1[i]=ch;
-            continue;
-        }
-        else if(s1!=s2){
-            break;
-        }
-                  }
+//         string s1=palindrome;
+//         int i;
+// for( i=0 ;i<n;i++){
+//     if(s1[i]!='a'){
+//         char ch=s1[i];
+//         s1[i]='a';
+//         string s2=s1;
+//         reverse(s2.begin(),s2.end());
+//         if(s1==s2){
+//             s1[i]=ch;
+//             continue;
+//         }
+//         else if(s1!=s2){
+//             break;
+//         }
+//                   }
    
-}
-       if(i==n)(s1[n-1]='b');
-        return s1;
+// }
+//        if(i==n)(s1[n-1]='b');
+//         return s1;
+        
+        
+        for(int i=0;i<n/2;i++){
+            if(palindrome[i]!='a'){
+                palindrome[i]='a';
+                return palindrome;
+                
+            }
+        }
+        palindrome[n-1]='b';
+        return palindrome;
     }
 };
