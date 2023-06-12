4a6fa9611faa39bbf718a3ae6175b652436d775a
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-06-12 16:56:15 +0530

    Time: 7 ms (48.45%), Space: 7.3 MB (74.80%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        vector<char>s1;
-      
-        for(int i=0;s[i]!='\0';i++){
-            char x=s[i];
-            if((x>='a'&& x<='z') || (x>='0' && x<='9')){
-                s1.push_back(x);
-               
+        int n = s.size(), l = 0, r = n - 1;
+        while (l < r) {
+            while (l < r && !isalnum(s[l])) {
+                l++;
             }
-            else if(x>='A'&& x<='Z'){
-               char ch=x;
-                ch+=32;
-                s1.push_back(ch);
+            while (l < r && !isalnum(s[r])) {
+                r--;
             }
-            else{
-                continue;
+            if (tolower(s[l++]) != tolower(s[r--])) {
+                return false;
             }
-            
         }
-        vector<char>s2(s1.begin(),s1.end());
-        
-        reverse(s1.begin(),s1.end());
-        if(s2==s1)return true;
-        return false;
+        return true;
+
         
     }
 };
