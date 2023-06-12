f5190399cc43fa8498533e7c662378bceea11959
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-06-12 18:31:25 +0530

    Time: 9 ms (28.45%), Space: 9.3 MB (40.00%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,44 +1,16 @@
-bool compare(string a ,string b){
-    return a.size()<b.size();
-}
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string>& v) {
         string ans="";
-        if(strs.size()==1){
-            string s1=strs[0];
-            return s1;
-        }
-        sort(strs.begin(),strs.end(),compare);
-        int i=0,j=0;
-        while(strs[0][i]!='\0' && strs[1][j]!='\0'){
-            if(strs[0][i]==strs[1][j]){
-                ans+=strs[0][i];
-                i++;
-                j++;
+        sort(v.begin(),v.end());
+        int n=v.size();
+        string first=v[0],last=v[n-1];
+        for(int i=0;i<min(first.size(),last.size());i++){
+            if(first[i]!=last[i]){
+                return ans;
             }
-            else break;
-        }
-        cout<<ans<<endl;
-        if(ans=="")return "";
-        
-        for(int i=2;i<strs.size();i++){
-            int k=0;int j=0;
-            if(strs[i]=="")return "";
-            while(ans[j]!='\0' and strs[i][k]!='\0'){
-                if(ans[0]!=strs[i][0])return "";
-                if(ans[j]==strs[i][k]){
-                    k++;j++;
-                    continue;
-                }
-                else if(ans[j]!=strs[i][j]){
-                    ans=ans.substr(0,j);
-                    break;
-                }
-            }
+            ans+=first[i];
         }
         return ans;
-        
-    
     }
 };
