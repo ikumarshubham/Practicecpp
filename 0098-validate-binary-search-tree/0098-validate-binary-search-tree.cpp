c8ce69fdf44f4e335f0245e95c9758f63460719e
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-08-15 10:00:33 +0530

    Time: 7 ms (91.25%), Space: 21.7 MB (29.62%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -9,28 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-void ans(TreeNode* root,vector<int>&vec1){
+bool isBST(TreeNode* root,long long minv=LLONG_MIN,long long maxv=LLONG_MAX){
     if(root==NULL){
-        return;
+        return true;
     }
-    ans(root->left,vec1);
-    vec1.push_back(root->val);
-    ans(root->right,vec1);
+    
+    if((root->val > minv && root->val< maxv) && isBST(root->left,minv,root->val) && isBST(root->right,root->val,maxv)){
+        return true;
+    }
+    return false;
 }
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        if(root==NULL){
-            return true;
-        }
-        vector<int> vec1;
-        ans(root,vec1);
-        int size=vec1.size();
-       for(int i=1;i<size;i++){
-           if(vec1[i]<=vec1[i-1]){
-               return false;
-           }
-       }
-        return true;
+       //  if(root==NULL){
+       //      return true;
+       //  }
+       //  vector<int> vec1;
+       //  ans(root,vec1);
+       //  int size=vec1.size();
+       // for(int i=1;i<size;i++){
+       //     if(vec1[i]<=vec1[i-1]){
+       //         return false;
+       //     }
+       // }
+       //  return true;
+        
+        return isBST(root);
     }
 };
