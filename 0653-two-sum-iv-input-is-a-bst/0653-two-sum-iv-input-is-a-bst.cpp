ec17439d4ba708308b797a7d7e84bf1b89965508
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-08-20 11:37:35 +0530

    Time: 30 ms (86.09%), Space: 36.8 MB (70.55%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,45 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class BSTIterator {
+    stack<TreeNode *> myStack;
+    bool reverse = true; 
+public:
+    BSTIterator(TreeNode *root, bool isReverse) {
+        reverse = isReverse; 
+        pushAll(root);
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !myStack.empty();
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        TreeNode *tmpNode = myStack.top();
+        myStack.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
+    }
+
+private:
+    void pushAll(TreeNode *node) {
+        for(;node != NULL; ) {
+             myStack.push(node);
+             if(reverse == true) {
+                 node = node->right; 
+             } else {
+                 node = node->left; 
+             }
+        }
+    }
+};
 class Solution {
 public:
-    void inorder(TreeNode* root,vector<int>& vec1){
-        if(root==NULL){
-            return;
-        }
-        inorder(root->left,vec1);
-        vec1.push_back(root->val);
-        inorder(root->right,vec1);
-    }
     bool findTarget(TreeNode* root, int k) {
-        vector<int>vec1;
-        inorder(root,vec1);
+        if(!root) return false; 
+        BSTIterator l(root, false); 
+        BSTIterator r(root, true); 
         
-        int i=0,j=vec1.size()-1;
-        
-        while(i<j){
-            if(vec1[i]+vec1[j]==k){
-                return true;
-            }
-            
-            else if(vec1[i]+vec1[j]<k){
-                i++;
-            }
-            else{
-                j--;
-            }
+        int i = l.next(); 
+        int j = r.next(); 
+        while(i<j) {
+            if(i + j == k) return true; 
+            else if(i + j < k) i = l.next(); 
+            else j = r.next(); 
         }
-        
-        return false;
-        
-        
+        return false; 
     }
 };
