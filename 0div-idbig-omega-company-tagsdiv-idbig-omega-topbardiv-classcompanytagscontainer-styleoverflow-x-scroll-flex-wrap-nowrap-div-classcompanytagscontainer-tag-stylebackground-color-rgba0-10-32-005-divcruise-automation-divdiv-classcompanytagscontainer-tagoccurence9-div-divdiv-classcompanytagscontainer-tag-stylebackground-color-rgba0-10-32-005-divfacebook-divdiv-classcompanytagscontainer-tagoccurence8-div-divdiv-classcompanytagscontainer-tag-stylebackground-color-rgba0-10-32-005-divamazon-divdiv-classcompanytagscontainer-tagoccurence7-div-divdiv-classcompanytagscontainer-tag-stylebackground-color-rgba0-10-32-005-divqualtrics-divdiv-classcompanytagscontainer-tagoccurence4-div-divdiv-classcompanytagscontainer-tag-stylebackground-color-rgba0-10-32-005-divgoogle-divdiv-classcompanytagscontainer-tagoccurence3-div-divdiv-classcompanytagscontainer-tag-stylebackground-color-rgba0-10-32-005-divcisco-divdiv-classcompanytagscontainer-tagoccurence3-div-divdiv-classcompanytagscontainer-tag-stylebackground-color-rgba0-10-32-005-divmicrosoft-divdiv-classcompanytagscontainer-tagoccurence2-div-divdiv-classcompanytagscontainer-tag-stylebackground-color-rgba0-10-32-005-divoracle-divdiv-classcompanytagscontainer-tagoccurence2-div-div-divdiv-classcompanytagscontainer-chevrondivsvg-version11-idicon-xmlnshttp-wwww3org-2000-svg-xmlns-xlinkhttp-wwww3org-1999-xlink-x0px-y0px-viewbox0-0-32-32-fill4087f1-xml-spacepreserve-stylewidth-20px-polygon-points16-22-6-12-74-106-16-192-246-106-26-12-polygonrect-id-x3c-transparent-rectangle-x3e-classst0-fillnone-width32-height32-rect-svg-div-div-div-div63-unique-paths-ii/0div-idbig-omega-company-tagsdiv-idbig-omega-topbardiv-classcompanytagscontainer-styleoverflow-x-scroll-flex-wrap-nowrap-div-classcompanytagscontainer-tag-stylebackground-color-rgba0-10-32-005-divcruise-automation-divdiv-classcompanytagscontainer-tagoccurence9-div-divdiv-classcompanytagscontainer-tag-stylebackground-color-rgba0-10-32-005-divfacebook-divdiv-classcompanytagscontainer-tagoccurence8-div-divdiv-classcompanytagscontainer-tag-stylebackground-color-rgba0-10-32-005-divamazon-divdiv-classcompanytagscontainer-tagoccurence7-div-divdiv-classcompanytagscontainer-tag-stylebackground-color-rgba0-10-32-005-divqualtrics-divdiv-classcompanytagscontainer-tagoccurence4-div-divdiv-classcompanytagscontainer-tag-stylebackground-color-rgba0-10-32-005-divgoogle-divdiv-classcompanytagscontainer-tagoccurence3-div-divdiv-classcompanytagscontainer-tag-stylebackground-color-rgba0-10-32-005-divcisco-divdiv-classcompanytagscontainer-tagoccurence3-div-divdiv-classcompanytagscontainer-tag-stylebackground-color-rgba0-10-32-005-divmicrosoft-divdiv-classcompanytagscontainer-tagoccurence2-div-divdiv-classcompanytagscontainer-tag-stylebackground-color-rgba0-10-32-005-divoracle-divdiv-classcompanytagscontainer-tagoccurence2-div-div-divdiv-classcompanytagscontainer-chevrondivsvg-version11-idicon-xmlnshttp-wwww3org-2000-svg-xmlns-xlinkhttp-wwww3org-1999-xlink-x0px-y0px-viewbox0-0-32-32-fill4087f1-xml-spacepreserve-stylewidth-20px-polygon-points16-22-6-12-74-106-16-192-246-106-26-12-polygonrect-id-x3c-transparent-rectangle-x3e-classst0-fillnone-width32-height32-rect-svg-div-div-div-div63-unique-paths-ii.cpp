bb23b4baa33c0e45aa6197a678012efa659a7f0f
ikumarshubham <131544932+ikumarshubham@users.noreply.github.com>
2023-12-15 11:52:34 +0530

    Time: 6 ms (10.26%), Space: 8.5 MB (5.30%) - LeetHub

---
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcruise-automation-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divqualtrics-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div63-unique-paths-ii/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcruise-automation-divdiv-classcompanytagscontainer-tagoccurence9-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divfacebook-divdiv-classcompanytagscontainer-tagoccurence8-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence7-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divqualtrics-divdiv-classcompanytagscontainer-tagoccurence4-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divcisco-divdiv-classcompanytagscontainer-tagoccurence3-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divmicrosoft-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divoracle-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div63-unique-paths-ii.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+//Template 
+#define ll long long 
+#define max1(a,b,c) max(a,max(b,c))
+#define pb push_back
+#define pop1 pop_back
+#define umap unordered_map
+#define uset unordered_set
+#define lb lower_bound
+#define ub upper_bound
+#define all(v) (v).begin(),(v).end()
+#define sort0(v) sort(all(v))
+#define sort0(v) sort(all(v))
+#define lower(s)  for (auto& x : s) { x = tolower(x); } 
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef pair<ll, ll> pll;
+
+const long long mod = 1e9 + 7;
+#define inf 1000000000000000005
+
+
+
+// functions
+ll inv(ll i) {if (i == 1) return 1; return (mod - ((mod / i) * inv(mod % i)) % mod) % mod;}
+
+ll mod_mul(ll a, ll b) {a = a % mod; b = b % mod; return (((a * b) % mod) + mod) % mod;}
+
+ll mod_add(ll a, ll b) {a = a % mod; b = b % mod; return (((a + b) % mod) + mod) % mod;}
+
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b);}
+
+ll ceil_div(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+
+ll pwr(ll a, ll b) {a %= mod; ll res = 1; while (b > 0) {if (b & 1) res = res * a % mod; a = a * a % mod; b >>= 1;} return res;}
+
+//
+
+
+
+//Template End
+
+
+
+
+
+
+class Solution {
+public:
+    
+    ll solve(vector<vi>&obstacleGrid,ll m,ll n,ll i,ll j,vector<vi>&DP){
+    if(i==m-1 and j==n-1){
+        
+        return 1;
+    }
+        
+
+    if(i>=m or j>=n)return 0;
+        
+    if(obstacleGrid[i][j]==1)return 0;
+
+    if(DP[i][j]!=-1)return DP[i][j];
+
+
+    ll ans =solve(obstacleGrid,m,n,i+1,j,DP)+solve(obstacleGrid,m,n,i,j+1,DP);
+
+
+    return DP[i][j]=ans;
+
+
+
+}
+
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
+        if(obstacleGrid.size()==0)return 0;
+        
+        ll m=obstacleGrid.size();
+        
+        ll n=obstacleGrid[0].size();
+        
+        
+         vector<vi>DP;
+
+        for(int i=0;i<=m;i++){
+            vi v;
+            for(int j=0;j<=n;j++){
+                v.pb(-1);
+            }
+
+            DP.pb(v);
+        }
+        
+        
+        if(obstacleGrid[m-1][n-1]==1)return 0;
+        
+        
+        return solve(obstacleGrid,m,n,0,0,DP);
+        
+        
+        
+        
+    }
+};
